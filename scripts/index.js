b4bea0b0f91b868ef5c5eef36898591e4b46ea92
Kate Deogenova <e.deogenova@yandex.ru>
2024-01-17 11:09:00 +0300

    mesto-project 1701241108

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,24 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+const placesList = document.querySelector('.places__list');
+const cardTemplate = document.querySelector('#card-template').content;
+
+function delCard (evt) {
+  const parentBttnDelCard = evt.target.closest('.places__item');
+  parentBttnDelCard.remove();
+}
+
+function createCard(cards, funDelCard) {
+  cards.forEach(detailsCard => {
+    const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
+    cardElement.querySelector('.card__image').src = `${detailsCard.link}`;
+    cardElement.querySelector('.card__image').alt = `Фото: ${detailsCard.name}`;
+    cardElement.querySelector('.card__title').textContent = `${detailsCard.name}`;
+    cardElement.querySelector('.card__delete-button').addEventListener('click', funDelCard);
+    placesList.append(cardElement);
+  })
+}
+
+createCard(initialCards, delCard);
